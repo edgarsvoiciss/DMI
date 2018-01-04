b2fc849fe23393f293f475c3137fd47c5e0196be
roberts.mukans@gmail.com <voicissedgars@gmail.com>
2018-01-04 18:41:34 +0200

    20180401_18_41

---
+++ PYTHON/labdarbs2.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+
+from math import cosh
+import numpy as np
+
+
+print "            500"
+print "        -----------"
+print "        \ "
+print "         \                  2*k"
+print "          \                X"
+print "cosh(x/2)=|    -----------------------------"
+print "          |                     2*k"
+print "          /            (2*k)!*2"
+print "         / "
+print "        / "
+print "        -----------"
+print "            k=0"
+
+
+
+print "                         2"                     
+print "                        x"
+print "            R= --------------------"
+print "                    2"
+print "                   2*(2k-1)*2k"
+
+
+def mans_cosh (x):
+      k = 0
+      a = 
+
+
+
+
+
+
+
